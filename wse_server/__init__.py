17f84e6a40dbdd59fa50c92f61fc33a7904d6272
silvermpx <silver.mpx@me.com>
2026-02-23 08:42:08 +0100

    Bump version to 1.3.5

--- wse_server/__init__.py
+++ wse_server/__init__.py
@@ -2,5 +2,5 @@
 
 from .router import SnapshotProvider, WSEConfig, create_wse_router
 
-__version__ = "1.3.4"
+__version__ = "1.3.5"
 __all__ = ["create_wse_router", "WSEConfig", "SnapshotProvider"]
